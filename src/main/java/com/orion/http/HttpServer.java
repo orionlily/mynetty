150235276523ec055f4c8276c0709b9a34a0ee7c
orionlily <gdoullc@163.com>
2021-03-09 00:32:00 +0800

    HTTP

---
+++ src/main/java/com/orion/http/HttpServer.java
@@ -0,0 +1,77 @@
+package com.orion.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author li.lc
+ */
+public class HttpServer {
+    private int port;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    //.localAddress(new InetSocketAddress(999))
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast("codec", new HttpServerCodec())
+                                    //.addLast("aggregator", new HttpObjectAggregator(512 * 1024))    //
+                                    .addLast(new HttpServerHandler());
+
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            System.out.println("准备启动服务器...");
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync(); // (7)
+            f.addListener(future -> {
+                if (future.isSuccess()) {
+                    System.out.println("服务器正在启动....");
+                }
+                if (future.isDone()) {
+                    System.out.println("服务器启动完成....");
+                }
+            });
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+            f.addListener(future ->  {
+                    if (future.isCancelled()) {
+                        System.out.println("服务器正在关闭..");
+                    }
+                    if (future.isCancellable()) {
+                        System.out.println("服务器已经关闭..OK");
+                    }
+            });
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new HttpServer(8080).run();
+    }
+}
